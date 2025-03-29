e8b439a512d88a49b2c0689532875232c3c8bb7c
ionatan_21 <barriosjonatan1221@gmail.com>
2025-03-28 20:08:36 -0600

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Biseccion from "./components/Biseccion/Biseccion";
 import Navbar from "./components/Navbar/Navbar";
 import Secante from "./components/Secante/Secante";
 import ReglaFalse from "./components/Regla-falsa/ReglaFalse";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Analytics />
       <Routes>
         <Route path="/" element={<Biseccion />} />
         <Route path="/secante" element={<Secante />} />
